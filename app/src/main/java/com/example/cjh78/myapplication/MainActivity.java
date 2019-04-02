413a0da0677168390af4e44b243f7eb124fbcb88
cjh7894 <cjh7894@gmail.com>
2019-04-02 19:47:21 +0200

    first commit

---
+++ app/src/main/java/com/example/cjh78/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.cjh78.myapplication;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
